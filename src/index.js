e1c3f25624b58333e2b0177703ed211c2f3c685d
woonzf <137765412+woonzf@users.noreply.github.com>
2024-04-05 21:13:45 +0800

    Add game loop and test with AI vs AI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,45 @@
-const Board = require('./board.js');
+const Player = require('./player.js');
 
 const battleship = (() => {
+    let name = null;
+    let player1 = null;
+    let player2 = null;
+    let turn = null;
+    let notTurn = null;
+
     function run() {
-        const board = new Board();
-        console.log(board.mapShips);
+        name = "Player 1";
+        player1 = new Player(name);
+        player2 = new Player("Player 2");
+        turn = player1;
+        notTurn = player2;
+
+        // Game loop
+        while (1) {
+            // AI vs AI
+            const map = notTurn.board.mapAttacks;
+            const attack = turn.giveAttack(map);
+            notTurn.receiveAttack(attack);
+
+            console.log(`${player1.name}: ${player1.life}, ${player2.name}: ${player2.life}`)
+            
+            if (isGame()) break;
+            updateTurn();
+        }
+
+        console.log(`${turn.name} wins!`)
+    }
+
+    function isGame() {
+        if (!Boolean(player1.life)) return true;
+        if (!Boolean(player2.life)) return true;
+        return false;
+    }
+
+    function updateTurn() {
+        const temp = turn;
+        turn = notTurn;
+        notTurn = temp;
     }
 
     return { run };
